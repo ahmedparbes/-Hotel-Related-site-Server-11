9e4be93a4e341ad9d40e652d3934cc7a49e8828a
ahmedparbes <fbhelpsbd@gmail.com>
2021-10-29 15:15:23 +0600

    add

--- server.js
+++ server.js
@@ -24,19 +24,53 @@ async function run() {
         await client.connect();
         const database = client.db("HotelDB");
         const hotelCollection = database.collection("hotels");
+        const userCollection = database.collection("users");
         // Post API
         app.post('/hotels', async (req, res) => {
             const hotels = req.body;
             const result = await hotelCollection.insertOne(hotels);
             res.send(hotels);
-        })
+        });
+
         // FIND
 
         app.get('/hotels', async (req, res) => {
             const cursors = hotelCollection.find({});
             const result = await cursors.toArray()
             res.send(result);
+        });
+
+
+        // USER API
+
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            const result = await userCollection.insertOne(users);
+            res.send(result);
+        });
+
+        app.get('/users', async (req, res) => {
+            const cursors = userCollection.find({});
+            const result = await cursors.toArray();
+            res.send(result);
+        });
+
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await userCollection.findOne(query);
+            res.send(result);
         })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.json(result)
+        })
+
+
+
 
         app.listen(port, () => {
             console.log('run with', port)
